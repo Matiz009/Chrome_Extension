4752ce9931777b7b1523379dc670724a680c7d25
MATI UL REHMAN <iammatiz00@gmail.com>
2022-08-14 01:16:59 +0500

    started

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ let btn = document.getElementById("btn-el");
 const myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
+let listOfItems = "";
 btn.addEventListener("click", function() {
     let input = inputEl.value;
     console.log("Clicked");
     myLeads.push(input);
     console.log(myLeads);
-    for (let i = 0; i < myLeads.length; i++) {
-        ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
-    }
+    renderItems();
+    inputEl.value = "";
 });
+
+function renderItems() {
+    for (let i = 0; i < myLeads.length; i++) {
+        listOfItems += "<li>" + myLeads[i] + "</li>";
+        ulEl.innerHTML = listOfItems;
+    }
+}
